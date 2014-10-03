b80aed4ccbe6eedf4b0cfd37da469f898b98c98d
Peter Krieg <copperkrieg@gmail.com>
2014-10-02 20:09:18 -0400

    added new folder for front end dev track

---
+++ Front End Development Track/js/test.js
@@ -0,0 +1,59 @@
+// alert('hello world!!!');
+
+
+// console.log("this should appear in google chrome dev tools!!");
+
+// var friends=["jack", "jim", 3, 4, 4.5, 5, 6];
+
+
+// for (i=0; i<friends.length; i++){
+// 	console.log(friends[i]);
+// }
+
+
+// var something = {
+// 	blah: friends[i],
+// 	name: "peter",
+// 	age: 18
+// }
+
+
+// var ID = document.getElementById('header');
+
+// ID.style.background = "green";
+// ID.style.color = "yellow";
+
+
+// var myVar;
+
+// console.log(typeof myVar === "undefined");
+
+
+
+
+
+
+// var otherVar = null;
+
+
+// if (otherVar == null){
+// 	console.log("If");
+// }    else {
+// 	console.log("Else");
+// }
+
+
+
+
+var world = "WORLD!!";
+
+function writestuff(){
+	var hello = "HELLO";
+	console.log(hello + world);
+}
+
+writestuff();
+console.log(world);
+console.log(hello);
+
+
