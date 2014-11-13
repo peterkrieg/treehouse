1f22394e385b8fa7bf10b07256e581c874b83b40
Peter Krieg <copperkrieg@gmail.com>
2014-11-12 21:57:32 -0500

    coderbyte factorial challenge

---
+++ Front End Development Track/js/test3.js
@@ -0,0 +1,21 @@
+
+//  factorial coderbyte "challenge"
+
+function firstFactorial(num){
+	n = num;
+	// takes into account the fact that 0! = 1
+	if(n===0){
+		num=1;
+	}
+	// every other situation accounted for
+	else{
+		for(var i =1; i<n; i++){
+		num = num*(n-i)
+		}
+	}
+	return num;
+}
+
+
+var userNum = parseInt(prompt('what would you like to take factorial of? '));
+alert('the factorial of ' + userNum + ' is ' + firstFactorial(userNum));
