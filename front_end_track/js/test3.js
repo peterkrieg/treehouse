0544e6d7359b3298353dcc952b5dc08b3bfb7587
Peter Krieg <copperkrieg@gmail.com>
2014-12-28 15:15:06 -0500

    started more practice on CSS, going over all treehouse videos

--- front_end_track/js/test3.js
+++ front_end_track/js/test3.js
@@ -1,6 +1,9 @@
 
 //  factorial coderbyte "challenge"
 
+
+/*----------------------------
+
 function firstFactorial(num){
 	n = num;
 	// takes into account the fact that 0! = 1
@@ -16,6 +19,23 @@ function firstFactorial(num){
 	return num;
 }
 
-
 var userNum = parseInt(prompt('what would you like to take factorial of? '));
 alert('the factorial of ' + userNum + ' is ' + firstFactorial(userNum));
+
+--------------------------------*/
+
+
+var array = ['hello', 'something', 'blah'];
+
+
+
+console.log(array.length);
+
+for(var i = 1; i<4; i++){
+	console.log(array.pop());
+}
+
+
+
+
+
