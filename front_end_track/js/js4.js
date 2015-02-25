3e1b5f39e4ebc3cb1c1c29209cefe38b5f346312
Peter Krieg <copperkrieg@gmail.com>
2015-02-25 12:16:31 -0500

    continued practicing jquery and sass, starting jquery basics course treehouse

---
+++ front_end_track/js/js4.js
@@ -0,0 +1,35 @@
+$(function() {
+	$('header p').hide().fadeIn(400);
+	$('section p').hide().fadeIn(800);
+	$('footer p').hide().fadeIn(3000);
+
+	$footerp = $('footer p');
+	footertext = $footerp.html();
+	console.log(footertext);
+
+	$footerp.prepend(' this is cool')
+	.append('what what')
+	.prepend('heyooo');
+
+
+
+
+	$listitem = $('section.sec1 li');
+
+	$listitem.each(function() {
+		$(this).append(' and it is a cool item ');
+	});
+
+	$listitem.on('click', function(e) {
+		console.log(e.type);
+		if(!$listitem.hasClass('color')){
+			$listitem.addClass('color');
+		}
+		else{
+			$listitem.removeClass('color');
+		}
+	});
+
+
+
+});
