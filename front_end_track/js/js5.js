b6990def200befb25265899af4da9745e945f53b
Peter Krieg <copperkrieg@gmail.com>
2015-02-27 14:37:18 -0500

    added new files, to practice more jQuery with treehouse course

---
+++ front_end_track/js/js5.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	$('.button').on('click', function(){
+		$('.safe').hide().html('<p> snape kills dumbledore </p>').fadeIn(1000);
+		$('.button').hide();
+	})
+
+
+
+
+
+});
