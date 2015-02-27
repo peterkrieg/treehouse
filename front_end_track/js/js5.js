70017003e118356536bd5175f37cdded2fe76528
Peter Krieg <copperkrieg@gmail.com>
2015-02-27 18:55:23 -0500

    finished 3rd stage of jquery basics treehouse, built basic lightbox

--- front_end_track/js/js5.js
+++ front_end_track/js/js5.js
@@ -2,8 +2,40 @@ $(document).ready(function(){
 
 	$('.button').on('click', function(){
 		$('.safe').hide().html('<p> snape kills dumbledore </p>').fadeIn(1000);
-		$('.button').hide();
-	})
+		$(this).hide();
+	});
+
+
+
+
+
+
+	// Image overlay part
+
+	var $overlay = $('.sec2 .overlay');
+	$overlay.hide();
+
+
+	$('#imgGallery a img').on('click', function(e){
+		console.log($(this).attr('alt'));
+		e.preventDefault();
+		$('.sec2').append($overlay);
+		$overlay.slideToggle(500).addClass('visible');
+		var $pic = $(this);
+		$('.picture').append($pic.clone());
+		$('.caption').append($(this).attr('alt'));
+
+
+
+		if($overlay.hasClass('visible')){
+			$('.close').on('click', function(){
+				$overlay.slideUp(500);
+				$('.picture').html('');
+				$('.caption').html('');
+			})
+		}
+	});
+
 
 
 
