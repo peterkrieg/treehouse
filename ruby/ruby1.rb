70a0d0592a37339b9d61173c2f7630b66d4a80c1
Peter Krieg <copperkrieg@gmail.com>
2015-03-15 12:23:07 -0400

    added toDoApp rails app

---
+++ ruby/ruby1.rb
@@ -0,0 +1,38 @@
+# print "please enter name: "
+# name =  gets
+# puts "Hello #{name}!"
+
+
+
+# string = <<-STRING 
+# this is a string
+# what a cool string
+# STRING
+
+# puts string
+
+# puts "This a string \t and a tab  \nin it"
+
+
+
+# petername = "PeterKrieg"
+# puts petername.upcase
+# puts petername.downcase
+# puts petername.reverse
+
+# a = 2
+# b = 5
+
+# puts a+b
+
+# puts "the sum of a and b, mult by 2 is #{(a+b)*2}"
+
+# Stage 4 creating ruby methods
+
+def add(a, b)
+	puts a+b
+	return 6
+end
+
+add(2, 3)
+
