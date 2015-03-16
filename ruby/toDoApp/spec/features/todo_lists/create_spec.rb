071bf98af2bbedab95a9a3ba2abf63e7f18f1cbc
Peter Krieg <copperkrieg@gmail.com>
2015-03-15 22:48:34 -0400

    added validation for description

--- ruby/toDoApp/spec/features/todo_lists/create_spec.rb
+++ ruby/toDoApp/spec/features/todo_lists/create_spec.rb
@@ -53,6 +53,26 @@ describe "Creating todo lists" do
 
 
 	end
+
+	it "displays an error when todo list has no description" do
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		click_link "New Todo list"
+		expect(page).to have_content("New todo_list")
+
+		fill_in "Title", with:"Hi"
+		fill_in "Description", with: ""
+		click_button "Create Todo list"
+
+		expect(page).to have_content("error")
+		expect(TodoList.count).to eq(0)
+
+		visit "/todo_lists"
+		expect(page).to_not have_content("This is what I'm doing today")
+
+
+	end
 end
 
 
